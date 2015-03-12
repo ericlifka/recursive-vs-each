20c317e4b65c5943d75240b54c25494415355cc0
Eric Lifka <eric.lifka@gmail.com>
2015-03-11 23:21:16 -0400

    properties to control render method

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,16 @@
 import Ember from 'ember';
 
-IndexController = Ember.Controller.extend({
+let IndexController = Ember.Controller.extend({
+    renderMethod: 'iterative',
+
+    recursiveRenderSelected: function () {
+        return this.get('renderMethod') === 'recursive';
+    }.property('renderMethod'),
+
+    iterativeRenderSelected: function () {
+        return this.get('renderMethod') === 'iterative';
+    }.property('renderMethod'),
+
 
 });
 
