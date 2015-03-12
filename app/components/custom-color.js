9c96f400d5b662d251fc235630ecf80e03c31ca4
Eric Lifka <eric.lifka@gmail.com>
2015-03-12 11:28:14 -0400

    custom render component

---
+++ app/components/custom-color.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+
+const CustomColorComponent = Ember.Component.extend({
+    items: null,
+
+    renderObserver: function () {
+        this.rerender();
+    }.observes('items.@each.color'),
+
+    render(buffer) {
+        console.log('render');
+        const items = this.get('items');
+        if (items) {
+            items.forEach(item => buffer.push(this.getMarkup(item)));
+        }
+    },
+
+    getMarkup(item) {
+        return `
+            <div class="color-component" style="background-color: ${item.get('color')};">
+                    ${item.get('id')}
+            </div>
+        `;
+    }
+});
+
+export default CustomColorComponent;
