b6cdafae5699afb93bc2c4312dd4b38ee786ed78
Eric Lifka <eric.lifka@gmail.com>
2015-03-12 00:21:44 -0400

    re-org

--- app/components/color-component.js
+++ app/components/color-component.js
@@ -2,8 +2,8 @@ import Ember from 'ember';
 
 const ColorComponent = Ember.Component.extend({
     attributeBindings: ['style'],
-
     item: null,
+
     style: function () {
         return `background-color: ${this.get('item.color')};`
     }.property('item.color')
