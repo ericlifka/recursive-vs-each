6cb9aa04a63c4ce419dedeecbf0fb5afde72cf4b
Eric Lifka <eric.lifka@gmail.com>
2015-03-14 19:09:37 -0400

    pull out generators as individual functions

--- app/utils/generators.js
+++ app/utils/generators.js
@@ -12,19 +12,24 @@ const dataArray = function (size) {
     return arr;
 };
 
+const iterativeData = size => dataArray(size);
+const customData = size => dataArray(size);
+
+const recursiveData = size =>
+    dataArray(size)
+        .map((current, index, collection) => current.set('next', collection[index + 1]))
+        .get(0);
+
+const cachedData = size =>
+    dataArray(size)
+        .map(item => item.set(
+            'template',
+            `<div class="color-component" style="background-color: ${item.get('color')};">${item.get('id')}</div>`
+        ));
+
 export default Ember.Object.create({
-    iterativeData: size => dataArray(size),
-    customData: size => dataArray(size),
-
-    recursiveData: size =>
-        dataArray(size)
-            .map((current, index, collection) => current.set('next', collection[index + 1]))
-            .get(0),
-
-    cachedData: size =>
-        dataArray(size)
-            .map(item => item.set(
-                'template',
-                `<div class="color-component" style="background-color: ${item.get('color')};">${item.get('id')}</div>`
-            ))
+    iterativeData,
+    customData,
+    recursiveData,
+    cachedData
 });
