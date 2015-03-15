628a20849010dee8b49cb577de02b139bfd9076b
Eric Lifka <eric.lifka@gmail.com>
2015-03-14 21:57:37 -0400

    switch to a simpler generator api

--- app/utils/generators.js
+++ app/utils/generators.js
@@ -2,7 +2,15 @@ import Ember from 'ember';
 
 const randomColor = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 
-const createObj = id => Ember.Object.create({ id, color: randomColor() });
+const createObj = id => Ember.Object.create({
+    id,
+    color: randomColor(),
+    template: `
+        <div class="color-component" style="background-color: ${randomColor()};">
+            ${id}
+        </div>
+    `
+});
 
 const dataArray = size => {
     const arr = [];
@@ -12,24 +20,10 @@ const dataArray = size => {
     return arr;
 };
 
-const iterativeData = dataArray;
-const customData = dataArray;
-
-const recursiveData = size =>
+const dataList = size =>
     dataArray(size)
         .map((current, index, collection) =>
             current.set('next', collection[index + 1]))
         .get(0);
 
-const cachedData = size =>
-    dataArray(size)
-        .map(item => item.set(
-            'template',
-            `<div class="color-component" style="background-color: ${item.get('color')};">${item.get('id')}</div>`));
-
-export default Ember.Object.create({
-    iterativeData,
-    customData,
-    recursiveData,
-    cachedData
-});
+export default Ember.Object.create({ dataArray, dataList });
