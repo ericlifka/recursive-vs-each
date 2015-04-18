3c8b7d12945adf4e451fa6732e983f49e172e6f3
Eric Lifka <eric.lifka@gmail.com>
2015-04-17 22:46:13 -0400

    enable babel es7 decorators module

--- Brocfile.js
+++ Brocfile.js
@@ -2,7 +2,11 @@
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp();
+var app = new EmberApp({
+  babel: {
+    optional: ['es7.decorators']
+  }
+});
 
 app.import('vendor/browser-polyfill.js');
 
